b4daa8d5052e07a983010e703f7e40bf611c1411
duanyubin <duanyubin@bytedance.com>
2018-07-26 17:31:26 +0800

    deploy

---
+++ precache-manifest.518e50270f033b18e37a2598bef872a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/pwa/sw.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa/robots.txt"
+  },
+  {
+    "revision": "970cb5f01811f8a42e23",
+    "url": "/pwa/js/chunk-vendors.970cb5f0.js"
+  },
+  {
+    "revision": "16a0e87109059aa07251",
+    "url": "/pwa/js/app.16a0e871.js"
+  },
+  {
+    "revision": "ae5a28ca407638cc7e49397ce134fe37",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "1c2a6701e586d093ba4afbb95139a761",
+    "url": "/pwa/img/256.1c2a6701.png"
+  },
+  {
+    "revision": "16a0e87109059aa07251",
+    "url": "/pwa/css/app.0f20c0c4.css"
+  }
+];
